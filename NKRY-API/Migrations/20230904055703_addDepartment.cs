ceeec09bdd7f1562ecdf2d12281d9bbf4d1639b5
Kilvny <108346469+Kilvny@users.noreply.github.com>
2023-09-04 09:03:04 +0300

    Foreign key fixed

---
+++ NKRY-API/Migrations/20230904055703_addDepartment.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NKRY_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class addDepartment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DepartmentId",
+                table: "users",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Department",
+                columns: table => new
+                {
+                    DepartmentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DepartmentName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Department", x => x.DepartmentId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_DepartmentId",
+                table: "users",
+                column: "DepartmentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_users_Department_DepartmentId",
+                table: "users",
+                column: "DepartmentId",
+                principalTable: "Department",
+                principalColumn: "DepartmentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_users_Department_DepartmentId",
+                table: "users");
+
+            migrationBuilder.DropTable(
+                name: "Department");
+
+            migrationBuilder.DropIndex(
+                name: "IX_users_DepartmentId",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "DepartmentId",
+                table: "users");
+        }
+    }
+}
