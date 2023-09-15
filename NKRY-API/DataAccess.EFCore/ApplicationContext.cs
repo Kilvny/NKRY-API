d79afcab38b26e06897a9ab1496d988f9092abd8
Kilvny <108346469+Kilvny@users.noreply.github.com>
2023-09-15 17:01:00 +0300

    feat: 0.1 working on IdentityDbContext

--- NKRY-API/DataAccess.EFCore/ApplicationContext.cs
+++ NKRY-API/DataAccess.EFCore/ApplicationContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using NKRY_API.Domain.Entities;
 
 namespace NKRY_API.DataAccess.EFCore
 {
-    public class ApplicationContext : DbContext
+    public class ApplicationContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationContext(DbContextOptions options) : base(options)
         {
